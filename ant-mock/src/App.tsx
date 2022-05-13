8b9ee47350831c95d0b6ce1a6d9c3dde35b8e185
yinchen <974578444@qq.com>
2022-05-13 21:51:27 +0800

    feat:menu初步框架

--- ant-mock/src/App.tsx
+++ ant-mock/src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button';
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
 const App: React.FC =() => {
   return (
     <div className="App">
@@ -7,6 +9,11 @@ const App: React.FC =() => {
         <Button disabled>hello</Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>hello</Button>
         <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled>Baidu</Button>
+        <Menu defaultIndex={0}>
+          <MenuItem>
+            cool link
+          </MenuItem>
+        </Menu>
         <a
           className="App-link"
           href="https://reactjs.org"
