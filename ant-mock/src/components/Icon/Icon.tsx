104dceca2fef009be5f6bb16d62f5755b5f46540
yinchen <974578444@qq.com>
2022-05-18 23:36:36 +0800

    睡前写完

--- ant-mock/src/components/Icon/Icon.tsx
+++ ant-mock/src/components/Icon/Icon.tsx
@@ -1,1 +1,5 @@
-import React from "react";
+import React from "react"; 
+import classNames from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export type ThemeProps = 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark'
