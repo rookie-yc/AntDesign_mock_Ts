e7778b5c3d972d4bc89fd5cb4c2d273100e11a81
yinchen <974578444@qq.com>
2022-05-19 23:55:52 +0800

    1

--- ant-mock/src/components/Icon/Icon.tsx
+++ ant-mock/src/components/Icon/Icon.tsx
@@ -1,5 +1,15 @@
 import React from "react"; 
 import classNames from "classnames";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome";
 
 export type ThemeProps = 'primary' | 'secondary' | 'success' | 'info' | 'warning' | 'danger' | 'light' | 'dark'
+
+export interface IconProps extends FontAwesomeIconProps {
+    theme? : ThemeProps
+}
+
+const Icon: React.FC<IconProps> = (props) => {
+    //icon-primary
+    const { className, theme, ...resetProps } = props
+
+}
