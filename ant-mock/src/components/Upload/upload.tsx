874592400811cdcdb96d28e9cc29fbcb651dd929
yinchen <974578444@qq.com>
2022-05-27 02:47:35 +0800

    feat：完成uplist

--- ant-mock/src/components/Upload/upload.tsx
+++ ant-mock/src/components/Upload/upload.tsx
@@ -1,23 +1,55 @@
-import React, { FC, useRef, ChangeEvent } from "react";
+import React, { FC, useRef, useState, ChangeEvent } from "react";
 import axios from "axios";
-
 import Button from "../Button/button";
+import UploadList from "./uploadList";
+import './_style.scss'
 
+export type UploadFileStatus = 'ready' | 'uploading' | 'success' | 'error'
+export interface UploadFile {
+    uid: string;
+    size: number;
+    name: string;
+    status?: UploadFileStatus;
+    percent?: number;
+    raw?: File;
+    response?: any;
+    error?: any;
+}
 export interface UploadProps {
     action: string;
+    defaultFileList?: UploadFile[];
+    beforeUpload?: (file: File) => boolean | Promise<File>
     onProgress?: (precentange: number, file: File) => void;
     onSuccess?: (data: any, file: File) => void;
     onError?: (err: any, file: File) => void;
+    onChange?: (file: File) => void;
+    onRemove?: (file: UploadFile) => void;
 }
 export const Upload: FC<UploadProps> = (props) => {
     const {
         action,
+        defaultFileList,
+        beforeUpload,
         onProgress,
         onSuccess,
-        onError
+        onError,
+        onChange, 
+        onRemove,
     } = props
 
     const fileInput = useRef<HTMLInputElement>(null)
+    const [ fileList, setFileList ] = useState<UploadFile[]>(defaultFileList || [])
+    const updateFileList = (updateFile: UploadFile, updateObj: Partial<UploadFile>) => {
+        setFileList(prevList => {
+            return prevList.map(file => {
+                if (file.uid === updateFile.uid) {
+                    return { ...file, ...updateObj }
+                  } else {
+                    return file
+                  }
+            })
+        })
+    }
     const handleClick = () => {
         if (fileInput.current) {
             fileInput.current.click()
@@ -33,38 +65,78 @@ export const Upload: FC<UploadProps> = (props) => {
             fileInput.current.value = ''
         }
     }
+    const handleRemove = (file: UploadFile) => {
+        setFileList((prevList) => {
+          return prevList.filter(item => item.uid !== file.uid)
+        })
+        if (onRemove) {
+          onRemove(file)
+        }
+    }
     const uploadFiles = (files: FileList) => {
         let postFiles = Array.from(files)
         postFiles.forEach(file => {
-            const formData = new FormData()
-            formData.append(file.name, file)
-            axios.post(action, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                    'Content-Security-Policy': 'upgrade-insecure-requests'
-                },
-                onUploadProgress: (e) => {
-                    let percentage = Math.round((e.loaded * 100) / e.total) || 0;
-                    if (percentage < 100) {
-                    //   updateFileList(_file, { percent: percentage, status: 'uploading'})
-                      if (onProgress) {
-                        onProgress(percentage, file)
-                      }
-                    } 
+            if (!beforeUpload) {
+                post(file)
+            } else {
+                const result = beforeUpload(file)
+                if (result && result instanceof Promise) {
+                    result.then(processedFile => {
+                        post(processedFile)
+                    })
+                } else if (result !== false) {
+                    post(file)
                 }
-            }).then(resp => {
-                console.log(resp)
-                if (onSuccess) {
-                    onSuccess(resp.data, file)
-                }
-            }).catch(err => {
-                console.error(err)
-                if(onError) {
-                    onError(err, file)
-                }
-            })
+            }      
         })
     }
+    const post = (file: File) => {
+        let _file: UploadFile = {
+            uid: Date.now() + 'upload-file',
+            status: 'ready',
+            name: file.name,
+            size: file.size,
+            percent: 0,
+            raw: file
+        }
+        const formData = new FormData()
+        formData.append(file.name, file)
+        axios.post(action, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+                'Content-Security-Policy': 'upgrade-insecure-requests'
+            },
+            onUploadProgress: (e) => {
+                let percentage = Math.round((e.loaded * 100) / e.total) || 0;
+                if (percentage < 100) {
+                  updateFileList(_file, { percent: percentage, status: 'uploading'})
+                  if (onProgress) {
+                    onProgress(percentage, file)
+                  }
+                } 
+            }
+        }).then(resp => {
+            console.log(resp)
+            updateFileList(_file, { status: 'success', response: resp.data})
+            if (onSuccess) {
+                onSuccess(resp.data, file)
+            }
+            if (onChange) {
+                onChange(file)
+            }
+        }).catch(err => {
+            updateFileList(_file, { status: 'error', error: err})
+            console.error(err)
+            if(onError) {
+                onError(err, file)
+            }
+            if (onChange) {
+                onChange(file)
+            }
+        })
+    }
+    console.log(fileList);
+    
     return (
         <div
             className="mock-upload-component"
@@ -81,6 +153,10 @@ export const Upload: FC<UploadProps> = (props) => {
                 ref={fileInput}
                 onChange={handleFileChange}
                 type="file"
+            />
+            <UploadList 
+                fileList={fileList}
+                onRemove={handleRemove}
             />
         </div>
     )
